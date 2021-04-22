78dfb275da3996c23cdb490bd57001dcbb7822fc
Ah <Patapataboom@aliyun.com>
2021-04-23 01:05:07 +0800

    根本不知道发生什么事，摁是拷了一姐的pom就好了

--- src/main/scala/Apr_18/SparkHbaseRDD.scala
+++ src/main/scala/Apr_18/SparkHbaseRDD.scala
@@ -1,23 +1,16 @@
 package Apr_18
 
-import org.apache.hadoop.hbase.client.{Put, Result, Scan}
+import org.apache.hadoop.hbase.client.Result
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
-import org.apache.hadoop.hbase.mapreduce.TableOutputFormat
-import org.apache.hadoop.hbase.protobuf.ProtobufUtil
-
-import java.util.Base64
-//import org.apache.hadoop.hbase.mapred.TableOutputFormat
-
-import org.apache.hadoop.hbase.mapreduce.TableInputFormat
 import org.apache.hadoop.hbase.util.Bytes
-import org.apache.hadoop.hbase.{HBaseConfiguration, HConstants, TableName}
+import org.apache.hadoop.hbase.mapreduce.{TableInputFormat, TableOutputFormat}
+import org.apache.hadoop.hbase.HBaseConfiguration
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.{SparkConf, SparkContext}
 
 object SparkHbaseRDD {
   def main(args: Array[String]): Unit = {
-    val localhost = "192.168.7.98"        // 公司
-    //    val localhost = "192.168.101.61"        // 屋企
+//    val localhost = "192.168.7.98"        // 公司
+    val localhost = "192.168.101.61"        // 屋企
     val sparkSession = SparkSession.builder().appName("SparkToHBase").master("local[4]").getOrCreate()
     val sc = sparkSession.sparkContext
 
@@ -25,17 +18,25 @@ object SparkHbaseRDD {
 
     //创建HBase配置
     val hbaseConf = HBaseConfiguration.create()
-    hbaseConf.set(HConstants.ZOOKEEPER_QUORUM, localhost) //设置zookeeper集群，也可以通过将hbase-site.xml导入classpath，但是建议在程序里这样设置
-    hbaseConf.set(HConstants.ZOOKEEPER_CLIENT_PORT, "2181") //设置zookeeper连接端口，默认2181
-    hbaseConf.set(TableOutputFormat.OUTPUT_TABLE, tableName)
-
-    val scan = new Scan()
-    scan.addFamily(Bytes.toBytes("cf"))
-    val proto = ProtobufUtil.toScan(scan)
-    val scanToString = new String(Base64.getEncoder.encode(proto.toByteArray))
-    hbaseConf.set(org.apache.hadoop.hbase.mapreduce.TableInputFormat.SCAN, scanToString)
+    hbaseConf.set("hbase.zookeeper.quorum", localhost) //设置zookeeper集群，也可以通过将hbase-site.xml导入classpath，但是建议在程序里这样设置
+    hbaseConf.set("hbase.zookeeper.property.clientPort", "2181") //设置zookeeper连接端口，默认2181
+    hbaseConf.set(TableInputFormat.INPUT_TABLE, tableName)
 
     //读取数据并转化成rdd
-    val hbaseRDD = sc.newAPIHadoopRDD(hbaseConf, classOf[org.apache.hadoop.hbase.mapreduce.TableInputFormat], classOf[ImmutableBytesWritable], classOf[Result])
+    val hbaseRDD = sc.newAPIHadoopRDD(hbaseConf,
+      classOf[org.apache.hadoop.hbase.mapreduce.TableInputFormat],
+      classOf[org.apache.hadoop.hbase.io.ImmutableBytesWritable],
+      classOf[org.apache.hadoop.hbase.client.Result])
+
+//    hbaseRDD.foreach{ case (_ ,result) =>
+//      //获取行键
+//      val key = Bytes.toString(result.getRow)
+//      //通过列族和列名获取列
+//      val name = Bytes.toString(result.getValue("cf".getBytes,"name".getBytes))
+//      val age = Bytes.toString(result.getValue("cf".getBytes,"gender".getBytes))
+//      val addr = Bytes.toString(result.getValue("cf".getBytes,"addr".getBytes))
+//      println("Row key:"+key+"\tcf.Name:"+name+"\tcf.Age:"+age+"\tcf.Addr:"+addr)
+//    }
+    sparkSession.stop()
   }
 }
